7ae92cf5d6473e817af0df6f891ce0894a2a63af
singlahimani786 <singlahimani786@gmail.com>
2024-12-28 17:56:54 +0530

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,85 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+import numpy as np
+from sort import *
+
+cap = cv2.VideoCapture('cars-video.mp4')
+model = YOLO("../Yolo-Weights/yolov8n.pt")
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+# Tracking
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+limits = [300, 697, 1273, 697]
+totalCount = []
+
+while True:
+    res, img = cap.read()
+    if not res:
+        print("End of video or cannot read the frame")
+        break
+
+    # Create an overlay
+    overlay = img.copy()
+    alpha = 0.4  # Transparency factor
+
+    # Draw the semi-transparent line on the overlay
+    cv2.line(overlay, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
+
+    # Blend the overlay with the original image
+    cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0, img)
+
+    results = model(img, stream=True)
+    detections = np.empty((0, 5))
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
+
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class
+            cls = int(box.cls[0])
+            currentClass = classNames[cls]
+            if currentClass == "car":
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+
+    for result in resultsTracker:
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        w, h = x2 - x1, y2 - y1
+        # Green bounding box
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(0, 255, 0))
+        cvzone.putTextRect(img, f' {int(id)}', (max(0, x1), max(35, y1)),
+                           scale=2, thickness=3, offset=10)
+
+        cx, cy = x1 + w // 2, y1 + h // 2
+
+        if limits[0] < cx < limits[2] and limits[1] - 15 < cy < limits[1] + 15:
+            if totalCount.count(id) == 0:
+                totalCount.append(id)
+
+    # Display total count
+    cv2.putText(img, str(len(totalCount)), (255, 100), cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 255), 8)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+cap.release()
+cv2.destroyAllWindows()
